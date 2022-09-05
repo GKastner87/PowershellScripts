f5abc268b4396932f882e4e09343c56596ab8888
GKastner87 <74800477+GKastner87@users.noreply.github.com>
2022-09-05 15:19:15 +1000

    Update CleanUp-OldAdobeFiles.ps1

--- CleanUp-OldAdobeFiles.ps1
+++ CleanUp-OldAdobeFiles.ps1
@@ -1,4 +1,15 @@
-﻿Start-Transcript "$($env:ProgramData)\Microsoft\IntuneManagementExtension\CustomScript\Logs\AdobeCleanup.log"
+<#
+Version: 1.0
+Author: Guy Kastner (guykastner.com)
+Script: InstallAdobePro.ps1
+Description:
+Waits 24 hours to clean up the Adobe Installer files from InstallAdobePro.ps1 script
+Release notes:
+Version 1.0: Original published version. 
+The script is provided "AS IS" with no warranties.
+#>
+
+Start-Transcript "$($env:ProgramData)\Microsoft\IntuneManagementExtension\CustomScript\Logs\AdobeCleanup.log"
 
 
 $fileObj = Get-Item -Path "C:\ProgramData\InTuneScripts\Adobe.zip"
