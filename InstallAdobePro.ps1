76daec9e0a0acba958ba4b3a879291a91659a89f
GKastner87 <74800477+GKastner87@users.noreply.github.com>
2022-09-05 15:18:03 +1000

    Update InstallAdobePro.ps1

--- InstallAdobePro.ps1
+++ InstallAdobePro.ps1
@@ -1,3 +1,16 @@
+<#
+Version: 1.0
+Author: Guy Kastner (guykastner.com)
+Script: InstallAdobePro.ps1
+Description:
+Downloads Adobe Pro installer locally and runs MSI exec to install
+
+Release notes:
+Version 1.0: Original published version. 
+The script is provided "AS IS" with no warranties.
+#>
+
+
 #Download Adobe installer
 $appname = "InstallAdobePro"
 $AdobeReg = "https://helpx.adobe.com/content/dam/help/en/x-productkb/global/update-operating-system-and-browser/jcr_content/root/content/flex/items/position/position-par/download_section/download-1/Fix-UpdateRequiredError.reg"
@@ -5,18 +18,15 @@ $AdobePro = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acr
 $AdobeInstalledPath = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 $Downloadfolder = "C:\ProgramData\InTuneScripts\Adobe\"
 $applogo = 'C:\ProgramData\Microsoft\IntuneManagementExtension\intune.png'
-Install-Module -Name BurntToast -force
-Copy-item "intune.png" -destination C:\ProgramData\Microsoft\IntuneManagementExtension
 Start-Transcript "$($env:ProgramData)\Microsoft\IntuneManagementExtension\CustomScript\Logs\$appname.log"
 
 
-#Download IE reg fix https://helpx.adobe.com/au/x-productkb/global/update-operating-system-and-browser.html?internal_browser=true
+#Download IE reg fix use only if getting prompted on launching the app https://helpx.adobe.com/au/x-productkb/global/update-operating-system-and-browser.html?internal_browser=true
 If (Test-Path 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg')
 { 
     echo 'Registry Already Downloaded'
 } else {
 Invoke-WebRequest -URI $AdobeReg -OutFile 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg'
-New-BurntToastNotification -Text "Adobe Acrobat Fix", 'Applying Registry Fix' -AppLogo $applogo
 Write-host 'Downloading Adobe Registry File from Web'
 Start-Sleep -seconds 15
 }
@@ -27,15 +37,12 @@ $process = Start-Process reg -ArgumentList "import C:\ProgramData\InTuneScripts\
 $process.ExitCode
 
 
-
-
 #Check for Download
 
 If (Test-Path $Downloadfolder) {
     echo 'Installer Already Downloaded'
 } Else {
     Invoke-WebRequest -URI $AdobePro -OutFile C:\ProgramData\InTuneScripts\Adobe.zip 
-    New-BurntToastNotification -Text "Downloading install files", 'Please wait, this may take 10 minutes' -AppLogo $Applogo
     Expand-Archive -Path C:\ProgramData\InTuneScripts\Adobe.zip -DestinationPath C:\ProgramData\InTuneScripts\Adobe\
         
 }
@@ -47,9 +54,7 @@ If (Test-Path $AdobeInstalledPath) {
     echo 'Adobe Pro already installed'
     exit
     } Else {
-    New-BurntToastNotification -Text "Adobe install", 'Commencing install' -AppLogo $applogo
     msiexec /i "C:\ProgramData\InTuneScripts\Adobe\Adobe Acrobat\AcroPro.msi" /qn
     }
-
 
 Stop-Transcript
