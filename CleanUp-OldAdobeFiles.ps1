4853a4feed8663cf7123824cbdb66c51f2e17442
GKastner87 <74800477+GKastner87@users.noreply.github.com>
2022-09-05 15:18:26 +1000

    Add files via upload

---
+++ CleanUp-OldAdobeFiles.ps1
@@ -0,0 +1,16 @@
+﻿Start-Transcript "$($env:ProgramData)\Microsoft\IntuneManagementExtension\CustomScript\Logs\AdobeCleanup.log"
+
+
+$fileObj = Get-Item -Path "C:\ProgramData\InTuneScripts\Adobe.zip"
+
+if (($fileObj.LastWriteTime) -lt (Get-Date).AddHours(-0.024)){
+
+Remove-item C:\ProgramData\InTuneScripts\Adobe.zip, C:\ProgramData\InTuneScripts\Adobe\, C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg -Recurse
+
+}
+
+else {
+Write-Output "File not old enough, waiting to remove"
+}
+
+Stop-Transcript
