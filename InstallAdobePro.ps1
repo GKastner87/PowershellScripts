27e7a14e60329ff084f162fdaf580ad38f62be81
Guy <74800477+GKastner87@users.noreply.github.com>
2023-11-30 07:23:42 +1100

    Update InstallAdobePro.ps1
    Added cleanup of the download folder after installation.

--- InstallAdobePro.ps1
+++ InstallAdobePro.ps1
@@ -1,60 +1,63 @@
 <#
-Version: 1.0
+Version: 1.1.0
 Author: Guy Kastner (guykastner.com)
 Script: InstallAdobePro.ps1
 Description:
-Downloads Adobe Pro installer locally and runs MSI exec to install
+Downloads Adobe Pro installer locally, runs MSI exec to install, and cleans up the download folder after installation.
 
 Release notes:
+Version 1.1.0: Added cleanup of the download folder after installation.
 Version 1.0: Original published version. 
 The script is provided "AS IS" with no warranties.
 #>
 
 
-#Download Adobe installer
+# Download Adobe installer
+
+# Define variables
 $appname = "InstallAdobePro"
 $AdobeReg = "https://helpx.adobe.com/content/dam/help/en/x-productkb/global/update-operating-system-and-browser/jcr_content/root/content/flex/items/position/position-par/download_section/download-1/Fix-UpdateRequiredError.reg"
 $AdobePro = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip"
 $AdobeInstalledPath = "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 $Downloadfolder = "C:\ProgramData\InTuneScripts\Adobe\"
 $applogo = 'C:\ProgramData\Microsoft\IntuneManagementExtension\intune.png'
+
+# Start transcript for logging
 Start-Transcript "$($env:ProgramData)\Microsoft\IntuneManagementExtension\CustomScript\Logs\$appname.log"
 
-
-#Download IE reg fix use only if getting prompted on launching the app https://helpx.adobe.com/au/x-productkb/global/update-operating-system-and-browser.html?internal_browser=true
-If (Test-Path 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg')
-{ 
+# Download IE reg fix use only if getting prompted on launching the app
+If (Test-Path 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg') { 
     echo 'Registry Already Downloaded'
 } else {
-Invoke-WebRequest -URI $AdobeReg -OutFile 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg'
-Write-host 'Downloading Adobe Registry File from Web'
-Start-Sleep -seconds 15
+    Invoke-WebRequest -URI $AdobeReg -OutFile 'C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg'
+    Write-host 'Downloading Adobe Registry File from Web'
+    Start-Sleep -seconds 15
 }
 
-#Install IE reg fix
-
+# Install IE reg fix
 $process = Start-Process reg -ArgumentList "import C:\ProgramData\InTuneScripts\Fix-UpdateRequiredError.reg" -PassThru -Wait
 $process.ExitCode
 
-
-#Check for Download
-
+# Check for Download
 If (Test-Path $Downloadfolder) {
     echo 'Installer Already Downloaded'
 } Else {
     Invoke-WebRequest -URI $AdobePro -OutFile C:\ProgramData\InTuneScripts\Adobe.zip 
-    Expand-Archive -Path C:\ProgramData\InTuneScripts\Adobe.zip -DestinationPath C:\ProgramData\InTuneScripts\Adobe\
-        
+    Expand-Archive -Path C:\ProgramData\InTuneScripts\Adobe.zip -DestinationPath C:\ProgramData\InTuneScripts\Adobe\        
 }
 
-#Wait for download and unzip
+# Wait for download and unzip
 Start-Sleep -Seconds 400
-#Install Adobe Pro
+
+# Install Adobe Pro
 If (Test-Path $AdobeInstalledPath) {
     echo 'Adobe Pro already installed'
-    exit
-    } Else {
-    msiexec /i "C:\ProgramData\InTuneScripts\Adobe\Adobe Acrobat\AcroPro.msi" /qn
-    }
+} Else {
+    Start-Process msiexec -ArgumentList "/i C:\ProgramData\InTuneScripts\Adobe\Adobe Acrobat\AcroPro.msi /qn" -Wait
 
+    # Clean up the download folder after installation
+    Remove-Item -Path "C:\ProgramData\InTuneScripts\Adobe\" -Recurse -Force
+}
+
+# Stop transcript
 Stop-Transcript
